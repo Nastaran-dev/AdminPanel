844bb34d949f5d7524c57dd2b467527d34887ddc
Nastaran-dev <mohammadinastaran1384@gmail.com>
2026-02-16 09:09:40 +0330

    feat(dashboard): add recent reviews summary and avatar group to CmBox3

--- src/app/dashboard/components/ComponentPage/CmBox3.jsx
+++ src/app/dashboard/components/ComponentPage/CmBox3.jsx
@@ -6,9 +6,16 @@ import { DataGrid } from "@mui/x-data-grid";
 import Paper from "@mui/material/Paper";
 import Image from "next/image";
 import Button from "@mui/material/Button";
-import VisibilityIcon from '@mui/icons-material/Visibility';
-import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import Typography from "@mui/material/Typography";
+import Rating from "@mui/material/Rating";
+import StarIcon from "@mui/icons-material/Star";
+import Avatar from "@mui/material/Avatar";
+import AvatarGroup from "@mui/material/AvatarGroup";
+import Link from "@mui/material/Link";
+import ArrowOutwardIcon from '@mui/icons-material/ArrowOutward';
 const columns = [
   { field: "id", headerName: "Customer", width: 90 },
   {
@@ -62,11 +69,18 @@ const columns = [
     renderCell: (params) => {
       const buttinPath = params.row.nacy;
       return (
-        <Box sx={{display:"flex" , gap:"10px" , alignItems:"center",height:"100%"}}>
-           <VisibilityIcon/>
-           <DeleteForeverIcon sx={{color:"red"}}/>
-          
-           <MoreVertIcon/>
+        <Box
+          sx={{
+            display: "flex",
+            gap: "10px",
+            alignItems: "center",
+            height: "100%",
+          }}
+        >
+          <VisibilityIcon />
+          <DeleteForeverIcon sx={{ color: "red" }} />
+
+          <MoreVertIcon />
         </Box>
       );
     },
@@ -79,7 +93,6 @@ const rows = [
     lastName: "James Carter",
     firstName: "/images/imgi_9_avatar9.webp",
     age: "	24 May 2024",
-  
   },
   {
     id: 2,
@@ -133,7 +146,21 @@ const rows = [
 
 const paginationModel = { page: 0, pageSize: 5 };
 
+const labels = {
+  0.5: "Useless",
+  1: "Useless+",
+  1.5: "Poor",
+  2: "Poor+",
+  2.5: "Ok",
+  3: "Ok+",
+  3.5: "Good",
+  4: "Good+",
+  4.5: "Excellent",
+  5: "Excellent+",
+};
+
 export default function CmBox3() {
+  const value = 3.5;
   const { dark } = useContext(myTheme);
   return (
     <Box
@@ -202,8 +229,101 @@ export default function CmBox3() {
           borderRadius: "10px",
           backgroundColor: dark ? "#1e2126" : "white",
           border: "0.1px solid #35383c",
+          padding: "10px 10px",
+          overflow: "hidden",
+          display: "flex",
+          flexDirection: "column",
+          gap: "30px",
         }}
-      ></Box>
+      >
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            padding: "0 10px",
+          }}
+        >
+          <Box sx={{ color: dark ? "white" : "black" }}>
+            <Typography>Recent Reviews</Typography>
+            <Typography sx={{ color: "gray" }}>2k+ 5 Star Review</Typography>
+          </Box>
+          <Link sx={{ color: "blue", cursor: "pointer",fontWeight:"600" }}>
+            View All
+            <ArrowOutwardIcon />
+          </Link>
+        </Box>
+        <Paper
+          sx={{
+            backgroundColor: dark ? "#1e1e1e" : "white",
+            borderRadius: "20px",
+            padding: "10px 10px",
+            m: 1,
+          }}
+        >
+          <Box sx={{ display: "flex", gap: "10px" }}>
+            {" "}
+            <Image
+              src="/images/imgi_4_avatar4.webp"
+              width={50}
+              height={30}
+              alt="hi"
+              style={{ borderRadius: "50%", objectFit: "cover" }}
+            ></Image>
+            <Box>
+              <Typography sx={{ color: dark ? "white" : "black" }}>
+                Antaly
+              </Typography>
+              <Box sx={{ width: 200, display: "flex", alignItems: "center" }}>
+                <Rating
+                  name="text-feedback"
+                  value={value}
+                  readOnly
+                  precision={0.5}
+                  emptyIcon={
+                    <StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />
+                  }
+                />
+              </Box>
+            </Box>
+          </Box>
+
+          <Typography
+            sx={{ color: dark ? "white" : "black", marginTop: "20px" }}
+          >
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima ab
+            ullam vitae consequatur quasi perspiciatis odio porro distinctio
+          </Typography>
+        </Paper>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-evenly",
+            alignItems: "center",
+          }}
+        >
+          <AvatarGroup
+            renderSurplus={(surplus) => <span>+{surplus.toString()[0]}k</span>}
+            total={4251}
+          >
+            <Avatar alt="Remy Sharp" src="/images/imgi_1_avatar1.webp" />
+            <Avatar alt="Travis Howard" src="/images/imgi_2_avatar2.webp" />
+            <Avatar alt="Agnes Walker" src="/images/imgi_3_avatar3.webp" />
+          </AvatarGroup>
+          <Box>
+            <Typography
+              sx={{ fontSize: { xs: "12px" }, color: dark ? "white" : "black" }}
+            >
+              2k+
+            </Typography>
+            <Typography
+              sx={{ fontSize: { xs: "8px" , sm:"12px" }, color: dark ? "white" : "black" }}
+            >
+              5 Star Review
+            </Typography>
+          </Box>
+          <Button variant="contained">Report</Button>
+        </Box>
+      </Box>
     </Box>
   );
 }
