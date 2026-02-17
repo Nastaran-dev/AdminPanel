03223d604d8a31ec1a01185cbf7b8f2d8fb5c806
Nastaran-dev <mohammadinastaran1384@gmail.com>
2026-02-17 09:42:49 +0330

    feat(employees): add Employees page with Buttons component and styled container

--- src/app/dashboard/Empliyees/Buttons.jsx
+++ src/app/dashboard/Empliyees/Buttons.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
-
+"use client"
+import React, { useContext } from "react";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import EditCalendarIcon from '@mui/icons-material/EditCalendar';
+import AddIcon from '@mui/icons-material/Add';
+import Typography from '@mui/material/Typography';
+import { myTheme } from "@/app/store/Store";
+import Divider from '@mui/material/Divider';
 export default function Buttons() {
+    const{dark}=useContext(myTheme)
   return (
-    <div>
-      fgfgfgh
-    </div>
-  )
+    <Stack spacing={2} sx={{display:"flex" , justifyContent:"space-between"}}>
+      <Box
+        sx={{ display: "flex", gap: "20px", justifyContent: "space-evenly" ,flexWrap:"wrap",alignItems:"center"}}
+      >
+         <Typography sx={{display:{xs:"none",lg:"flex"},color:dark?"white":"black"}}>Employee</Typography>
+         
+        <Button variant="contained" sx={{backgroundColor:"#9ec9ff",color:"#633dfe",fontWeight:"700","&:hover":{backgroundColor:"#633dfe","&:hover":{color:"#fff"}},display:"flex",gap:"5px",justifyContent:"space-between",fontSize:{xs:"10px",md:"15px"}}}><EditCalendarIcon/>Export Report</Button>
+        <Button variant="contained" sx={{backgroundColor:"#633dfe",color:"#fff",fontWeight:"700",display:"flex",gap:"5px",justifyContent:"space-between",fontSize:{xs:"10px",md:"15px"}}}><AddIcon/>Add Employee</Button>
+         <Button variant="contained" sx={{backgroundColor:"black",color:"#fff",fontWeight:"700",display:"flex",gap:"5px",justifyContent:"space-between",fontSize:{xs:"10px",md:"15px"}}}><AddIcon/>Invite Employee</Button>
+      </Box>
+    </Stack>
+  );
 }
--- src/app/dashboard/Empliyees/page.jsx
+++ src/app/dashboard/Empliyees/page.jsx
@@ -3,7 +3,7 @@ import Box from '@mui/material/Box';
 import Buttons from './Buttons';
 export default function page() {
   return (
-    <Box sx={{border:"1px solid gray",borderRadius:"10px"}}>
+    <Box sx={{border:"1px solid gray",borderRadius:"10px",p:2}}>
       <Buttons/>
     </Box>
   )
